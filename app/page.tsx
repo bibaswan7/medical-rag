112db8b3e96882d163b53101ed32f6bcb0f26470
Bibaswan Prasai <bibaswanprasai12@gmail.com>
2024-09-21 07:28:58 +0545

    fix: minor

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
-      User's landing page
+      User&apos;s landing page
     </main>
   );
 }
